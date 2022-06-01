63617c728ff7b46539d3b44c9bd6a4a96af25f80
GaiYu0 <yg1246@nyu.edu>
2022-06-01 09:29:56 -0700

    gpt2 w/ segmented candidates

--- is_next.py
+++ is_next.py
@@ -8,27 +8,32 @@ from pathlib import Path
 from typing import *
 
 import torch
+import torch.nn.functional as F
 import torch.multiprocessing as mp
 from transformers import *
 
 
 def reader(args, filenames: List[str], queue: mp.Queue):
-    kwargs = {}
-
     tokenizer = AutoTokenizer.from_pretrained(args.lm_card)
-    if "gpt2" in args.lm_card:
-        tokenizer.pad_token = tokenizer.eos_token
-        kwargs["pad_token_id"] = tokenizer.eos_token_id
 
     def put(text_pairs, records, queue):
         _, cands = zip(*text_pairs[-args.batch_size:])
-        inputs = tokenizer.batch_encode_plus(text_pairs[-args.batch_size:],
-                                             padding=True,
-                                             truncation=True,
-                                             max_length=512,
-                                             return_tensors="pt",
-                                             return_token_type_ids=True)
-        queue.put([records[-args.batch_size:], cands, inputs, kwargs])
+        if 'bert' in args.lm_card:
+            inputs = tokenizer.batch_encode_plus(text_pairs[-args.batch_size:],
+                                                 padding=True,
+                                                 truncation=True,
+                                                 max_length=512,
+                                                 return_tensors='pt')
+        elif 'gpt' in args.lm_card:
+            tokenizer.pad_token = tokenizer.eos_token
+            inputs = tokenizer.batch_encode_plus(text_pairs[-args.batch_size:],
+                                                 padding=True,
+                                                 truncation=True,
+                                                 max_length=512,
+                                                 return_tensors='pt',
+                                                 return_token_type_ids=True)
+
+        queue.put([records[-args.batch_size:], cands, inputs])
         del records[-args.batch_size:]
         del text_pairs[-args.batch_size:]
 
@@ -37,12 +42,12 @@ def reader(args, filenames: List[str], queue: mp.Queue):
     for filename in filenames:
         lines = open(filename).readlines()
         jsons = list(map(json.loads, lines))
-        num_cands = sum(len(js["candidates"]) for js in jsons)
+        num_cands = sum(len(js['candidates']) for js in jsons)
         for lineno, js in enumerate(jsons):
             records.extend(
-                repeat([filename, num_cands, lineno, js["page"], js["query"]],
-                       len(js["candidates"])))
-            text_pairs.extend([js["query"], cand] for cand in js["candidates"])
+                repeat([filename, num_cands, lineno, js['page'], js['query']],
+                       len(js['candidates'])))
+            text_pairs.extend([js['query'], cand] for cand in js['candidates'])
         while len(text_pairs) >= args.batch_size:
             put(text_pairs, records, queue)
 
@@ -54,30 +59,34 @@ def reader(args, filenames: List[str], queue: mp.Queue):
 
 
 def estimator(args, device, input_queue, output_queue):
-    if "bert" in args.lm_card:
+    if 'bert' in args.lm_card:
         lm = AutoModelForNextSentencePrediction.from_pretrained(args.lm_card)
-    elif "gpt" in args.lm_card:
+    elif 'gpt' in args.lm_card:
         lm = lm = GPT2LMHeadModel.from_pretrained(args.lm_card)
     else:
         raise ValueError()
     lm = lm.to(device)
 
     while True:
-        records, cands, inputs, kwargs = input_queue.get()
-        inputs = inputs.to(device)
-
-        with torch.no_grad():
-            outputs = lm(**inputs)  # (B * N, 2)
-
-        if "bert" in args.lm_card:
-            scores = outputs["logits"].softmax(1)[:, 0]  # (B * N,)
-        elif "gpt" in args.lm_card:
-            logits = outputs["logits"].log_softmax(2).gather(
-                2, inputs['input_ids'][:, :, None]).squeeze(2)
-            token_type_ids = inputs['token_type_ids']
-            mask = (token_type_ids
-                    == 1) & (token_type_ids.cumsum(1) <= args.win_size)
-            scores = logits.masked_fill(~mask, 0).sum(1) / mask.sum(1)
+        records, cands, inputs = input_queue.get()
+        if 'bert' in args.lm_card:
+            inputs = inputs.to(device)
+            with torch.no_grad():
+                outputs = lm(**inputs)
+                scores = outputs['logits'].softmax(1)[:, 0]
+        elif 'gpt' in args.lm_card:
+            inputs = inputs.to(device)
+            with torch.no_grad():
+                outputs = lm(**inputs)
+                token_type_ids = inputs['token_type_ids']
+                cumsum = token_type_ids.cumsum(1)
+                mask = F.pad((0 < cumsum) & (cumsum <= args.win_size),
+                             [-1, 1, 0, 0])
+                scores = outputs['logits'].log_softmax(2).gather(
+                    2,
+                    F.pad(inputs['input_ids'],
+                          [-1, 1, 0, 0])[:, :, None]).squeeze(2).masked_fill(
+                              ~mask, 0).sum(1) / mask.sum(1)
 
         for record, cand, score in zip(records, cands, scores):
             output_queue.put([record, cand, score.item()])
@@ -95,24 +104,24 @@ def writer(args, queue):
                 (jsonl[lineno] for lineno in sorted(jsonl)))))
 
 
-if __name__ == "__main__":
-    mp.set_start_method("spawn")
+if __name__ == '__main__':
+    mp.set_start_method('spawn')
     logging.set_verbosity_warning()
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--batch-size", type=int)
-    parser.add_argument("--gpus", type=int, nargs='+')
-    parser.add_argument("--in-dir", type=Path)
-    parser.add_argument("--lm-card",
+    parser.add_argument('--batch-size', type=int)
+    parser.add_argument('--gpus', type=int, nargs='+')
+    parser.add_argument('--in-dir', type=Path)
+    parser.add_argument('--lm-card',
                         type=str,
-                        default="allenai/scibert_scivocab_cased")
-    parser.add_argument("--num-readers", type=int)
-    parser.add_argument("--num-writers", type=int)
-    parser.add_argument("--out-dir", type=Path)
-    parser.add_argument("--win-size", type=int)
+                        default='allenai/scibert_scivocab_cased')
+    parser.add_argument('--num-readers', type=int)
+    parser.add_argument('--num-writers', type=int)
+    parser.add_argument('--out-dir', type=Path)
+    parser.add_argument('--win-size', type=int)
     args = parser.parse_args()
 
-    filenames = list(args.in_dir.glob("*.json"))
+    filenames = list(args.in_dir.glob('*.json'))
 
     download_queue = mp.Queue()
     input_queue = mp.Queue()
@@ -132,7 +141,7 @@ if __name__ == "__main__":
         mp.Process(target=estimator,
                    args=[
                        args,
-                       torch.device(f"cuda:{gpu}"), input_queue, output_queue
+                       torch.device(f'cuda:{gpu}'), input_queue, output_queue
                    ]) for gpu in args.gpus
     ]
 
@@ -147,17 +156,17 @@ if __name__ == "__main__":
     num_files = len(filenames)
     num_cands_by_file = defaultdict(int)
     jsons = defaultdict(lambda: defaultdict(lambda: {
-        "page": None,
-        "query": None,
-        "candidates": []
+        'page': None,
+        'query': None,
+        'candidates': []
     }))
     while num_files > 0:
         [filename, num_cands, lineno, page,
          query], cand, score = output_queue.get()
 
         js = jsons[filename][lineno]
-        js.update({"page": page, "query": query})
-        js["candidates"].append([cand, score])
+        js.update({'page': page, 'query': query})
+        js['candidates'].append([cand, score])
 
         num_cands_by_file[filename] += 1
         if num_cands_by_file[filename] == num_cands:
